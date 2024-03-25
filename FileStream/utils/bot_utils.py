8fce83cf6c649d98efd8efb6c72908e1d71a2971
DextiN <47779849+dikshitd5@users.noreply.github.com>
2024-03-26 00:11:44 +0530

    Update bot_utils.py

--- FileStream/utils/bot_utils.py
+++ FileStream/utils/bot_utils.py
@@ -88,8 +88,8 @@ async def gen_link(_id):
     file_size = humanbytes(file_info['file_size'])
     mime_type = file_info['mime_type']
 
-    page_link = f"{Server.URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
-    stream_link = f"{Server.URL}dl/{_id}"
+    page_link = f"{Server.URL}watch/{_id}/dextinbots"
+    stream_link = f"{Server.URL}dl/{_id}/dextinbots"
     file_link = f"https://t.me/{FileStream.username}?start=file_{_id}"
 
     if "video" in mime_type:
