d5915eb835df0c6ab9dd4235989e935b2e39d0aa
DextiN <47779849+dikshitd5@users.noreply.github.com>
2024-03-25 22:20:47 +0530

    Update translation.py

--- FileStream/utils/translation.py
+++ FileStream/utils/translation.py
@@ -28,13 +28,15 @@ class LANG(object):
 <b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <b>{}</b>\n
 <b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <code>{}</code>\n
 <b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> {}\n
-<b>🖥 Wᴀᴛᴄʜ :</b> {}\n"""
+<b>🖥 Wᴀᴛᴄʜ :</b> {}\n
+<b>🚸 Nᴏᴛᴇ : LINK WON'T EXPIRE TILL I DELETE</b>"""
 
     STREAM_TEXT_X = """
 <i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n
 <b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <b>{}</b>\n
 <b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> {}\n
-<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> {}\n"""
+<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> {}\n
+<b>🚸 Nᴏᴛᴇ : LINK WON'T EXPIRE TILL I DELETE</b>"""
 
 
     BAN_TEXT = "__Sᴏʀʀʏ Sɪʀ, Yᴏᴜ ᴀʀᴇ Bᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴍᴇ.__\n\n**[Cᴏɴᴛᴀᴄᴛ Dᴇᴠᴇʟᴏᴘᴇʀ](tg://user?id={}) Tʜᴇʏ Wɪʟʟ Hᴇʟᴘ Yᴏᴜ**"
